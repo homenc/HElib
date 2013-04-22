f007979cb479f3440c476f3c2c16eadb91be985c
Victor Shoup <shoup@cs.nyu.edu>
2013-04-22 15:06:24 -0400

    test.

--- src/FHEContext.h
+++ src/FHEContext.h
@@ -13,6 +13,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
+
 #ifndef _FHEcontext_H_
 #define _FHEcontext_H_
 /**
