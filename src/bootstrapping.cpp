f95c8035b2e02ad2b68240fdd5484be6d8e818f7
Shai Halevi <shaih@alum.mit.edu>
2014-09-02 18:46:12 -0400

    More timing data More rows in the mValues table

--- src/bootstrapping.cpp
+++ src/bootstrapping.cpp
@@ -22,28 +22,46 @@ NTL_CLIENT
 
 //#define DEBUGGING
 
-#define num_mValues 18
-static long mValues[num_mValues][12] = { 
- // {phi(m),  m,    d, m1,  m2, m3,   g1,    g2,    g3,ord1,ord2,ord3}
-  {  600,  1023, 10, 11,  93,  0,   838,   584,    0, 10,  6,   0}, // m=(3)*11*{31} m/phim(m)=1.7 C=24 D=2 E=1
-  {12800, 17425, 40, 41, 425,  0,  5951,  8078,    0, 40, -8,   0}, // m=(5^2)*{17}*41 :-( m/phim(m)=1.36 C=93 D=3 E=3
-  {15004, 15709, 22, 23, 683,  0,  4099, 13663,    0, 22, 31,   0}, // m=23*(683) m/phim(m)=1.04        C=73  D=2 E=1
-  {18000, 18631, 25, 31, 601,  0, 15627,  1334,    0, 30, 24,   0}, // m=31*(601) m/phim(m)=1.03        C=77  D=2 E=0
-  {18000, 20801, 60, 31, 671,  0, 18789, 19500,    0, 30, 10,   0}, // m=(11)*31*{61} m/phim(m)=1.15    C=98  D=2 E=2
-  {18816, 24295, 28, 43, 565,  0, 16386, 16427,    0, 42, 16,   0}, // m=(5)*43*{113} m/phim(m)=1.29    C=84  D=2 E=2
-  {21168, 27305, 28, 43, 635,  0, 10796, 26059,    0, 42, 18,   0}, // m=(5)*43*{127} m/phim(m)=1.28    C=86  D=2 E=2
-  {23040, 28679, 24, 17,  7, 241, 15184,  4098,28204, 16,  6, -10}, // m=7*17*(241) :-( m/phim(m)=1.24  C=63  D=4 E=3
-  {24000, 31775, 20, 41, 775,  0,  6976, 24806,    0, 40, 30,   0}, // m=(5^2)*{31}*41 m/phim(m)=1.32   C=88  D=2 E=2
-  {26400, 27311, 55, 31, 881,  0, 21145,  1830,    0, 30, 16,   0}, // m=31*(881) m/phim(m)=1.03        C=99  D=2 E=0
-  {31104, 35113, 36, 37, 949,  0, 16134,  8548,    0, 36, 24,   0}, // m=(13)*37*{73} m/phim(m)=1.12    C=94  D=2 E=2
-  {34848, 45655, 44, 23, 1985, 0, 33746, 27831,    0, 22, 36,   0}, // m=(5)*23*{397} m/phim(m)=1.31    C=100 D=2 E=2
-  {42336, 42799, 21, 127, 337, 0, 25276, 40133,    0,126, 16,   0}, // m=127*(337) m/phim(m)=1.01       C=161 D=2 E=0
-  {45360, 46063, 45, 73, 631,  0, 35337, 20222,    0, 72, 14,   0}, // m=73*(631) m/phim(m)=1.01        C=129 D=2 E=0
-  {46080, 53261, 24, 17, 13, 241, 43863, 28680,15913, 16, 12, -10}, // m=13*17*(241) :-( m/phim(m)=1.15 C=69  D=4 E=3
-  {49500, 49981, 30, 151, 331, 0,  6952, 28540,    0,150, 11,   0}, // m=151*(331) m/phim(m)=1          C=189 D=2 E=1
-  {54000, 55831, 25, 31, 1801, 0, 19812, 50593,    0, 30, 72,   0}, // m=31*(1801) m/phim(m)=1.03       C=125 D=2 E=0
-  {60016, 60787, 22, 89, 683,  0,  2050, 58741,    0, 88, 31,   0}  //  m=89*(683) m/phim(m)=1.01       C=139 D=2 E=1
+static long mValues[][13] = { 
+//{ p, phi(m),  m,    d, m1,  m2, m3,   g1,    g2,    g3,ord1,ord2,ord3}
+  {  2,   600,  1023, 10, 11,  93,  0,   838,   584,    0, 10,  6,   0}, // m=(3)*11*{31} m/phim(m)=1.7    C=24  D=2 E=1
+  {  2, 12800, 17425, 40, 41, 425,  0,  5951,  8078,    0, 40, -8,   0}, // m=(5^2)*{17}*41 m/phim(m)=1.36 C=93  D=3 E=3
+  {  2, 15004, 15709, 22, 23, 683,  0,  4099, 13663,    0, 22, 31,   0}, // m=23*(683) m/phim(m)=1.04      C=73  D=2 E=1
+  {  2, 18000, 18631, 25, 31, 601,  0, 15627,  1334,    0, 30, 24,   0}, // m=31*(601) m/phim(m)=1.03      C=77  D=2 E=0
+  {  2, 18816, 24295, 28, 43, 565,  0, 16386, 16427,    0, 42, 16,   0}, // m=(5)*43*{113} m/phim(m)=1.29  C=84  D=2 E=2
+  {  2, 21168, 27305, 28, 43, 635,  0, 10796, 26059,    0, 42, 18,   0}, // m=(5)*43*{127} m/phim(m)=1.28  C=86  D=2 E=2
+  {  2, 23040, 28679, 24, 17,  7, 241, 15184,  4098,28204, 16,  6, -10}, // m=7*17*(241) m/phim(m)=1.24    C=63  D=4 E=3
+  {  2, 24000, 31775, 20, 41, 775,  0,  6976, 24806,    0, 40, 30,   0}, // m=(5^2)*{31}*41 m/phim(m)=1.32 C=88  D=2 E=2
+  {  2, 26400, 27311, 55, 31, 881,  0, 21145,  1830,    0, 30, 16,   0}, // m=31*(881) m/phim(m)=1.03      C=99  D=2 E=0
+  {  2, 31104, 35113, 36, 37, 949,  0, 16134,  8548,    0, 36, 24,   0}, // m=(13)*37*{73} m/phim(m)=1.12  C=94  D=2 E=2
+  {  2, 34848, 45655, 44, 23, 1985, 0, 33746, 27831,    0, 22, 36,   0}, // m=(5)*23*{397} m/phim(m)=1.31  C=100 D=2 E=2
+  {  2, 42336, 42799, 21, 127, 337, 0, 25276, 40133,    0,126, 16,   0}, // m=127*(337) m/phim(m)=1.01     C=161 D=2 E=0
+  {  2, 45360, 46063, 45, 73, 631,  0, 35337, 20222,    0, 72, 14,   0}, // m=73*(631) m/phim(m)=1.01      C=129 D=2 E=0
+  {  2, 46080, 53261, 24, 17, 13, 241, 43863, 28680,15913, 16, 12, -10}, // m=13*17*(241) m/phim(m)=1.15   C=69  D=4 E=3
+  {  2, 49500, 49981, 30, 151, 331, 0,  6952, 28540,    0,150, 11,   0}, // m=151*(331) m/phim(m)=1        C=189 D=2 E=1
+  {  2, 54000, 55831, 25, 31, 1801, 0, 19812, 50593,    0, 30, 72,   0}, // m=31*(1801) m/phim(m)=1.03     C=125 D=2 E=0
+  {  2, 60016, 60787, 22, 89, 683,  0,  2050, 58741,    0, 88, 31,   0}, // m=89*(683) m/phim(m)=1.01      C=139 D=2 E=1
+
+  { 17,   576,  1365, 12,  7,   3, 65,   976,   911,  463,  6,  2,   4}, // m=3*(5)*7*{13} m/phim(m)=2.36  C=22  D=3
+  { 17, 18000, 21917, 30, 101, 217, 0,  5860,  5455,    0, 100, 6,  0}, // m=(7)*{31}*101 m/phim(m)=1.21  C=134 D=2 
+  { 17, 30000, 34441, 30, 101, 341, 0,  2729, 31715,    0, 100, 10,  0}, // m=(11)*{31}*101 m/phim(m)=1.14 C=138 D=2
+  { 17, 40000, 45551, 40, 101, 451, 0, 19394,  7677,    0, 100, 10,  0}, // m=(11)*{41}*101 m/phim(m)=1.13 C=148 D=2
+  { 17, 46656, 52429, 36, 109, 481, 0, 46658,  5778,    0, 108, 12,  0}, // m=(13)*{37}*109 m/phim(m)=1.12 C=154 D=2
+  { 17, 54208, 59363, 44, 23, 2581, 0, 25811,  5199,    0, 22, 56,   0}, // m=23*(29)*{89} m/phim(m)=1.09  C=120 D=2
+  { 17, 70000, 78881, 10, 101, 781, 0, 67167, 58581,    0, 100, 70,  0}, // m=(11)*{71}*101 m/phim(m)=1.12 C=178 D=2
+
+  {127,   576,  1365, 12,  7,   3, 65,   976,   911,  463,  6,  2,   4}, // m=3*(5)*7*{13} m/phim(m)=2.36   C=22  D=3
+  {127,  1200,  1925, 20,  11, 175, 0,  1751,   199,    0, 10, 6,    0}, //  m=(5^2)*{7}*11 m/phim(m)=1.6   C=34 D=2
+  {127,  2160,  2821, 30,  13, 217, 0,   652,   222,    0, 12, 6,    0}, // m=(7)*13*{31} m/phim(m)=1.3     C=46 D=2
+
+  {127, 18816, 24295, 28, 43, 565,  0, 16386, 16427,    0, 42, 16,   0}, // m=(5)*43*{113} m/phim(m)=1.29   C=84  D=2
+  {127, 26112, 30277, 24, 17, 1781, 0, 14249, 10694,    0, 16, 68,   0}, // m=(13)*17*{137} m/phim(m)=1.15  C=106 D=2
+  {127, 39168, 62335, 24, 7,  5, 1781, 17811, 37402,49876,  6,  4,  68}, // m=5*7*(13)*{137} m/phim(m)=1.59 C=100 D=3
+  {127, 49392, 61103, 28, 43, 1421, 0,  1422, 14234,    0, 42, 42,   0}, // m=(7^2)*{29}*43 m/phim(m)=1.23  C=110 D=2
+  {127, 54400, 61787, 40, 41, 1507, 0, 30141, 46782,    0, 40, 34,   0}, // m=(11)*41*{137} m/phim(m)=1.13  C=112 D=2
+  {127, 72000, 77531, 30, 61, 1271, 0,  7627, 34344,    0, 60, 40,   0}  // m=(31)*{41}*61 m/phim(m)=1.07   C=128 D=2
 };
+#define num_mValues (sizeof(mValues)/(13*sizeof(long)))
 
 
 /*********** Debugging utilities **************/
@@ -198,10 +216,13 @@ void extractDigitsPacked(Ctxt& ctxt, long botHigh, long r, long ePrime,
   bool foundError = false;
 #endif
   for (long i=0; i<(long)unpacked.size(); i++) {
-    if (topHigh>0) { // extract digits topHigh...0, store them in scratch
+    if (topHigh<=0) { // extracting LSB = no-op
+      scratch.assign(1,unpacked[i]);
+    } else {          // extract digits topHigh...0, store them in scratch
+      FHE_NTIMER_START(extractDigits_method);
       extractDigits(scratch, unpacked[i], topHigh+1);
-
-#ifdef DEBUGGING // check digit-extraction
+      FHE_NTIMER_STOP(extractDigits_method);
+#ifdef DEBUGGING      // check digit-extraction
       long nDigits = scratch.size();
       if (dbgKey && dbgEa && !foundError) {
 	vector<ZZX> slots;
@@ -231,11 +252,10 @@ void extractDigitsPacked(Ctxt& ctxt, long botHigh, long r, long ePrime,
 	}
       }
 #endif
-    } else {
-      scratch.assign(1,unpacked[i]); // extracting LSB = no-op
     }
 
     // set upacked[i] = -\sum_{j=botHigh}^{topHigh} scratch[j] * p^{j-botHigh}
+    FHE_NTIMER_START(combiningDigits);
     if (topHigh >= (long)scratch.size()) {
       topHigh = scratch.size() -1;
       cerr << " @ suspect: not enough digits in extractDigitsPacked\n";
@@ -264,6 +284,7 @@ void extractDigitsPacked(Ctxt& ctxt, long botHigh, long r, long ePrime,
 
     // Our plaintext space is now mod p^r
     unpacked[i].reducePtxtSpace(p2r);
+    FHE_NTIMER_STOP(combiningDigits);
   }
   tm += GetTime();
   cerr << "  Extracting in "<<tm<<" seconds.";
@@ -271,18 +292,22 @@ void extractDigitsPacked(Ctxt& ctxt, long botHigh, long r, long ePrime,
 
   // Step 3: re-pack the slots
   tm = -GetTime();
+  FHE_NTIMER_START(repackOverhead);
   EncryptedArray ea2(ctxt.getContext(), ctxt.getContext().alMod);
   long nSlots = ea2.size();
   vector<ZZX> xVec(nSlots, ZZX(1,1)); // X in all the slots
   ZZX xInSlots;
   ea2.encode(xInSlots, xVec);         // encode as ZZX
+  FHE_NTIMER_STOP(repackOverhead);
   ZZX x2iInSlots = xInSlots;          // X^i := X
 
   ctxt = unpacked[0];
   for (long i=1; i<d; i++) {
     unpacked[i].multByConstant(x2iInSlots);
+    FHE_NTIMER_START(repackOverhead);
     MulMod(x2iInSlots, x2iInSlots,xInSlots,ctxt.getContext().zMStar.getPhimX());
     PolyRed(x2iInSlots, conv<ZZ>(p2r));
+    FHE_NTIMER_STOP(repackOverhead);
     ctxt += unpacked[i];
   }
   tm += GetTime();
