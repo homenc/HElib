62464b233198b4e3f30af0025a20ef66dd7f26a1
Shai Halevi <shaih@alum.mit.edu>
2014-09-05 12:25:16 -0400

    Moved the linear-transformation implementation to matmul.cpp

--- src/AltEvalMap.cpp
+++ src/AltEvalMap.cpp
@@ -1,13 +1,26 @@
-
-
+/* Copyright (C) 2012-2014 IBM Corp.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+/* AltEvalMap.cpp - Implementing the reCrypt linear transformations
+ */
 namespace std {} using namespace std;
 namespace NTL {} using namespace NTL;
-
 #include "AltEvalMap.h"
 #include "powerful.h"
 
-
-
+// The callback interface for the matrix-multiplication routines.
 template<class type>
 class AltStep2Matrix : public PlaintextMatrixInterface<type> 
 {
