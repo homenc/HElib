57b098a0a3cf1246db3ff2baa67327dc7c1f11a0
Shai Halevi <shaih@alum.mit.edu>
2015-01-08 11:54:10 -0500

    .

--- src/PAlgebra.cpp
+++ src/PAlgebra.cpp
@@ -13,6 +13,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
+
 #include <algorithm>   // defines count(...), min(...)
 
 #include "PAlgebra.h"
