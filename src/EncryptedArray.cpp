022f10b312c53a5a39f1efaf202c04df10121383
Shai Halevi <shaih@alum.mit.edu>
2017-05-24 18:07:51 -0400

    bugfix

--- src/EncryptedArray.cpp
+++ src/EncryptedArray.cpp
@@ -9,7 +9,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. See accompanying LICENSE file.
  */
- */
 /* EncryptedArray.cpp - Data-movement operations on arrays of slots
  */
 
