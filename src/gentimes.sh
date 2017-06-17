8f34cd560e7cff83d3a74fed19f4a626eade8ee6
victorshoup <shoup@cs.nyu.edu>
2017-06-17 08:39:09 -0400

    .

---
+++ src/gentimes.sh
@@ -0,0 +1,14 @@
+./Test_matmul1D_x verbose=1 m=15709 L=10 g=26  2>&1
+echo
+echo "---------------"
+echo
+./Test_matmul1D_x verbose=1 m=5461 L=10 g=11  2>&1
+echo
+echo "---------------"
+echo
+./Test_matmul1D_x m=20801 verbose=1 L=10 g=5  2>&1
+echo
+echo "---------------"
+echo
+./Test_matmul1D_x verbose=1 m=10013 L=10 g=3  2>&1
+
