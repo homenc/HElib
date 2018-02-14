cdd83ba379653ec92db24c1a625baef9b4890fe2
Shai Halevi <shaih@us.ibm.com>
2018-02-14 15:13:32 -0500

    moretesting options for binaryArith

--- src/Test_binaryArith.cpp
+++ src/Test_binaryArith.cpp
@@ -35,27 +35,35 @@ static bool verbose=false;
 
 static long mValues[][15] = { 
 // { p, phi(m),   m,   d, m1, m2, m3,    g1,   g2,   g3, ord1,ord2,ord3, B,c}
-  {  2,    48,   105, 12,  3, 35,  0,    71,    76,    0,   2,  2,   0, 25, 2},
-  {  2 ,  600,  1023, 10, 11, 93,  0,   838,   584,    0,  10,  6,   0, 25, 2},
-  {  2,  2304,  4641, 24,  7,  3,221,  3979,  3095, 3760,   6,  2,  -8, 25, 3},
-  {  2, 15004, 15709, 22, 23,683,  0,  4099, 13663,    0,  22, 31,   0, 25, 3},
-  {  2, 27000, 32767, 15, 31,  7, 151, 11628, 28087,25824, 30,  6, -10, 28, 4}
+  {  2,    48,   105, 12,   3, 35,  0,    71,    76,    0,   2,  2,   0, 25, 2},
+  {  2 ,  600,  1023, 10,  11, 93,  0,   838,   584,    0,  10,  6,   0, 25, 2},
+  {  2,  2304,  4641, 24,   7,  3,221,  3979,  3095, 3760,   6,  2,  -8, 25, 3},
+  {  2,  5460,  8193, 26,8193,  0,  0,    46,     0,    0, 210,  0,   0, 25, 3},
+  {  2,  8190,  8191, 13,8191,  0,  0,    39,     0,    0, 630,  0,   0, 25, 3},
+  {  2, 10752, 11441, 48,  17,673,  0,  4712,  2024,    0,  16,-14,   0, 25, 3},
+  {  2, 15004, 15709, 22,  23,683,  0,  4099, 13663,    0,  22, 31,   0, 25, 3},
+  {  2, 27000, 32767, 15,  31,  7,151, 11628, 28087,25824,  30,  6, -10, 28, 4}
 };
 
 void test15for4(FHESecKey& secKey);
-void testProduct(FHESecKey& secKey, long bitSize, long outSize,
-                 bool bootstrap = false);
+void testProduct(FHESecKey& secKey, long bitSize1, long bitSize2,
+                 long outSize, bool bootstrap = false);
+void testAdd(FHESecKey& secKey, long bitSize1, long bitSize2,
+             long outSize, bool bootstrap = false);
 
 int main(int argc, char *argv[])
 {
   ArgMapping amap;
   long prm=1;
-  amap.arg("prm", prm, "parameter size (0-tiny,...,4-huge)");
+  amap.arg("prm", prm, "parameter size (0-tiny,...,7-huge)");
   long bitSize = 5;
   amap.arg("bitSize", bitSize, "bitSize of input integers (<=32)");
+  long bitSize2 = 0;
+  amap.arg("bitSize2", bitSize2, "bitSize of 2nd input integer (<=32)",
+           "same as bitSize");
   long outSize = 0;
   amap.arg("outSize", outSize, "bitSize of output integers", "as many as needed");
-  long nTests = 3;
+  long nTests = 2;
   amap.arg("nTests", nTests, "number of tests to run");
   bool bootstrap = false;
   amap.arg("bootstrap", bootstrap, "test multiplication with bootstrapping");
@@ -65,6 +73,9 @@ int main(int argc, char *argv[])
   amap.arg("nthreads", nthreads, "number of threads");
   amap.arg("verbose", verbose, "print more information");
 
+  long tests2avoid = 1;
+  amap.arg("tests2avoid", tests2avoid, "bitmap of tests to disable (1-15for4, 2-add, 4-multiply");
+
   amap.parse(argc, argv);
   assert(prm >= 0 && prm < 5);
   if (seed) NTL::SetSeed(ZZ(seed));
@@ -72,6 +83,8 @@ int main(int argc, char *argv[])
 
   if (bitSize<=0) bitSize=5;
   else if (bitSize>32) bitSize=32;
+  if (bitSize2<=0) bitSize2=bitSize;
+  else if (bitSize2>32) bitSize2=32;
 
   long* vals = mValues[prm];
   long p = vals[0];
@@ -108,7 +121,8 @@ int main(int argc, char *argv[])
   }
   
   if (verbose) {
-    cout <<"input bitSize="<<bitSize<<", output size bound="<<outSize
+    cout <<"input bitSizes="<<bitSize<<','<<bitSize2
+         <<", output size bound="<<outSize
          <<", running "<<nTests<<" tests for each function\n";
     if (nthreads>1) cout << "  using "<<NTL::AvailableThreads()<<" threads\n";
     cout << "computing key-independent tables..." << std::flush;
@@ -140,14 +154,21 @@ int main(int argc, char *argv[])
   dbgKey = &secKey;
 #endif
 
-  for (long i=0; i<nTests; i++)
-    test15for4(secKey);
-  cout << "  *** test15for4 PASS ***\n";
-  
-  for (long i=0; i<nTests; i++)
-    testProduct(secKey, bitSize, outSize, bootstrap);
-  cout << "  *** testProduct PASS ***\n";
-
+  if (!(tests2avoid & 1)) {
+    for (long i=0; i<nTests; i++)
+      test15for4(secKey);
+    cout << "  *** test15for4 PASS ***\n";
+  }
+  if (!(tests2avoid & 2)) {
+    for (long i=0; i<nTests; i++)
+      testAdd(secKey, bitSize, bitSize2, outSize, bootstrap);
+    cout << "  *** testAdd PASS ***\n";
+  }
+  if (!(tests2avoid & 4)) {
+    for (long i=0; i<nTests; i++)
+      testProduct(secKey, bitSize, bitSize2, outSize, bootstrap);
+    cout << "  *** testProduct PASS ***\n";
+  }
   if (verbose) printAllTimers(cout);
   return 0;
 }
@@ -174,8 +195,14 @@ void test15for4(FHESecKey& secKey)
   inputBits += ")";
 
   // Add these bits
+  if (verbose) {
+    cout << endl;
+    CheckCtxt(inBuf[lsize(inBuf)-1], "b4 15for4");
+  }
   long numOutputs
     = fifteenOrLess4Four(CtPtrs_vectorCt(outBuf), CtPtrs_vectorPt(inPtrs));
+  if (verbose)
+    CheckCtxt(outBuf[lsize(outBuf)-1], "after 15for4");
 
   // Check the result
   long sum2=0;
@@ -193,30 +220,38 @@ void test15for4(FHESecKey& secKey)
     cout << "15to4 succeeded, sum"<<inputBits<<"="<<sum2<<endl;
 }
 
-void testProduct(FHESecKey& secKey, long bitSize, long outSize,
-                 bool bootstrap)
+void testProduct(FHESecKey& secKey, long bitSize, long bitSize2,
+                 long outSize, bool bootstrap)
 {
   const EncryptedArray& ea = *(secKey.getContext().ea);
   long mask = (outSize? ((1L<<outSize)-1) : -1);
 
   // Choose two random n-bit integers
   long pa = RandomBits_long(bitSize);
-  long pb = RandomBits_long(bitSize);
+  long pb = RandomBits_long(bitSize2);
 
   // Encrypt the individual bits
   NTL::Vec<Ctxt> eProduct, enca, encb;
 
   resize(enca, bitSize, Ctxt(secKey));
-  resize(encb, bitSize, Ctxt(secKey));
   for (long i=0; i<bitSize; i++) {
     secKey.Encrypt(enca[i], ZZX((pa>>i)&1));
-    secKey.Encrypt(encb[i], ZZX((pb>>i)&1));
     if (bootstrap) { // put them at a lower level
       enca[i].modDownToLevel(5);
+    }
+  }
+  resize(encb, bitSize2, Ctxt(secKey));
+  for (long i=0; i<bitSize2; i++) {
+    secKey.Encrypt(encb[i], ZZX((pb>>i)&1));
+    if (bootstrap) { // put them at a lower level
       encb[i].modDownToLevel(5);
     }
   }
-
+  if (verbose) {
+    cout << "\n  bits-size "<<bitSize<<'+'<<bitSize2;
+    if (outSize>0) cout << "->"<<outSize;
+    CheckCtxt(encb[0], "b4 multiplication");
+  }
   // Test positive multiplication
   vector<long> slots;
   {CtPtrs_VecCt eep(eProduct);  // A wrappers around the output vector
@@ -224,6 +259,8 @@ void testProduct(FHESecKey& secKey, long bitSize, long outSize,
                  outSize, &unpackSlotEncoding);
   decryptBinaryNums(slots, eep, secKey, ea);
   } // get rid of the wrapper
+  if (verbose)
+    CheckCtxt(eProduct[lsize(eProduct)-1], "after multiplication");
   long pProd = pa*pb;
   if (slots[0] != ((pa*pb)&mask)) {
     cout << "Positive product error: pa="<<pa<<", pb="<<pb
@@ -237,7 +274,7 @@ void testProduct(FHESecKey& secKey, long bitSize, long outSize,
     cout << pa<<"*"<<pb<<"="<<slots[0]<<endl;
   }
   // Test negative multiplication
-  secKey.Encrypt(encb[bitSize-1], ZZX(1));
+  secKey.Encrypt(encb[bitSize2-1], ZZX(1));
   decryptBinaryNums(slots, CtPtrs_VecCt(encb), secKey, ea, /*negative=*/true);
   pb = slots[0];
   eProduct.kill();
@@ -246,6 +283,8 @@ void testProduct(FHESecKey& secKey, long bitSize, long outSize,
                  outSize, &unpackSlotEncoding);
   decryptBinaryNums(slots, eep, secKey, ea, /*negative=*/true);
   } // get rid of the wrapper
+  if (verbose)
+    CheckCtxt(eProduct[lsize(eProduct)-1], "after multiplication");
   pProd = pa*pb;
   if ((slots[0]&mask) != (pProd&mask)) {
     cout << "Negative product error: pa="<<pa<<", pb="<<pb
@@ -270,6 +309,77 @@ void testProduct(FHESecKey& secKey, long bitSize, long outSize,
     }
   }
   decryptAndPrint((cout<<" after multiplication: "), *minCtxt, secKey, ea,0);
+  cout << endl;
+#endif
+}
+
+
+void testAdd(FHESecKey& secKey, long bitSize1, long bitSize2,
+             long outSize, bool bootstrap)
+{
+  const EncryptedArray& ea = *(secKey.getContext().ea);
+  long mask = (outSize? ((1L<<outSize)-1) : -1);
+
+  // Choose two random n-bit integers
+  long pa = RandomBits_long(bitSize1);
+  long pb = RandomBits_long(bitSize2);
+
+  // Encrypt the individual bits
+  NTL::Vec<Ctxt> eSum, enca, encb;
+
+  resize(enca, bitSize1, Ctxt(secKey));
+  for (long i=0; i<bitSize1; i++) {
+    secKey.Encrypt(enca[i], ZZX((pa>>i)&1));
+    if (bootstrap) { // put them at a lower level
+      enca[i].modDownToLevel(5);
+    }
+  }
+  resize(encb, bitSize2, Ctxt(secKey));
+  for (long i=0; i<bitSize2; i++) {
+    secKey.Encrypt(encb[i], ZZX((pb>>i)&1));
+    if (bootstrap) { // put them at a lower level
+      encb[i].modDownToLevel(5);
+    }
+  }
+  if (verbose) {
+    cout << "\n  bits-size "<<bitSize1<<'+'<<bitSize2;
+    if (outSize>0) cout << "->"<<outSize;
+    cout <<endl;
+    CheckCtxt(encb[0], "b4 addition");
+  }
+
+  // Test addition
+  vector<long> slots;
+  {CtPtrs_VecCt eep(eSum);  // A wrapper around the output vector
+  addTwoNumbers(eep, CtPtrs_VecCt(enca), CtPtrs_VecCt(encb),
+                outSize, &unpackSlotEncoding);
+  decryptBinaryNums(slots, eep, secKey, ea);
+  } // get rid of the wrapper
+  if (verbose) CheckCtxt(eSum[lsize(eSum)-1], "after addition");
+  long pSum = pa+pb;
+  if (slots[0] != ((pa+pb)&mask)) {
+    cout << "addTwoNums error: pa="<<pa<<", pb="<<pb
+         << ", but pSum="<<slots[0]
+         << " (should be ="<<(pSum&mask)<<")\n";
+    exit(0);
+  }
+  else if (verbose) {
+    cout << "addTwoNums succeeded: ";
+    if (outSize) cout << "bottom "<<outSize<<" bits of ";
+    cout << pa<<"+"<<pb<<"="<<slots[0]<<endl;
+  }
+
+#ifdef DEBUG_PRINTOUT
+  const Ctxt* minCtxt = nullptr;
+  long minLvl=1000;
+  for (const Ctxt& c: eSum) {
+    long lvl = c.findBaseLevel();
+    if (lvl < minLvl) {
+      minCtxt = &c;
+      minLvl = lvl;
+    }
+  }
+  decryptAndPrint((cout<<" after addition: "), *minCtxt, secKey, ea,0);
   cout << endl;
 #endif
 }
