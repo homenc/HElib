b16a5d8ac427ca848bfee803eab736bb57840c93
Shai Halevi <shaih@us.ibm.com>
2018-06-06 08:39:10 -0400

    more norm functions

--- src/norms.cpp
+++ src/norms.cpp
@@ -12,9 +12,42 @@
 /**
  * @file norms.cpp - computing various norms of ring elements
  **/
+#include <numeric>
+#include <NTL/BasicThreadPool.h>
+#include "NumbTh.h"
+#include "DoubleCRT.h"
 #include "norms.h"
 NTL_CLIENT
 
+long sumOfCoeffs(const zzX& f) // = f(1)
+{
+  long sum = 0;
+  for (long i=0; i<lsize(f); i++) sum += f[i];
+  return sum;
+}
+ZZ sumOfCoeffs(const ZZX& f) // = f(1)
+{
+  ZZ sum = ZZ::zero();
+  for (long i=0; i<=deg(f); i++) sum += coeff(f,i);
+  return sum;
+}
+NTL::ZZ sumOfCoeffs(const DoubleCRT& f)
+{
+  ZZX poly;
+  f.toPoly(poly);
+  return sumOfCoeffs(poly);
+}
+
+
+long largestCoeff(const zzX& f) // l_infty norm
+{
+  long mx = 0;
+  for (long i=0; i<lsize(f); i++) {
+    if (mx < abs(f[i]))
+      mx = abs(f[i]);
+  }
+  return mx;
+}
 ZZ largestCoeff(const ZZX& f)
 {
   ZZ mx = ZZ::zero();
@@ -24,20 +57,132 @@ ZZ largestCoeff(const ZZX& f)
   }
   return mx;
 }
-
-ZZ sumOfCoeffs(const ZZX& f) // = f(1)
+NTL::ZZ largestCoeff(const DoubleCRT& f)
 {
-  ZZ sum = ZZ::zero();
-  for (long i=0; i<=deg(f); i++) sum += coeff(f,i);
-  return sum;
+  ZZX poly;
+  f.toPoly(poly);
+  return largestCoeff(poly);
 }
 
-xdouble coeffsL2Norm(const ZZX& f) // l_2 norm
+
+double coeffsL2NormSquared(const zzX& f) // l_2 norm square
 {
-  xdouble s = to_xdouble(0.0);
-  for (long i=0; i<=deg(f); i++) {
-    xdouble coef = to_xdouble(coeff(f,i));
+  double s = 0.0;
+  for (long i=0; i<lsize(f); i++) {
+    double coef = f[i];
     s += coef * coef;
   }
-  return sqrt(s);
+  return s;
+}
+xdouble coeffsL2NormSquared(const ZZX& f) // l_2 norm square
+{
+  xdouble s(0.0);
+  for (long i=0; i<=deg(f); i++) {
+    xdouble coef(conv<xdouble>(coeff(f,i)));
+    s += coef * coef;
+  }
+  return s;
+}
+xdouble coeffsL2NormSquared(const DoubleCRT& f) // l2 norm^2
+{
+  ZZX poly;
+  f.toPoly(poly);
+  return coeffsL2NormSquared(poly);
+}
+
+// An extremely lame implementation of approximating the canonical
+// embedding norm of a polynomial
+#include <complex>
+#include <cmath>
+
+// evaluate poly(x) using Horner's rule
+complex<double> complexEvalPoly(const zzX& poly, const complex<double>& x)
+{
+  if (lsize(poly)<=0) return complex<double>(0.0,0.0);
+  complex<double> res(double(poly[0]), 0.0);
+  for (long i=1; i<lsize(poly); i++) {
+    res *= x;
+    res += complex<double>(double(poly[i]));
+  }
+  return res;
+}
+complex<xdouble> complexEvalPoly(const ZZX& poly, const complex<double>& x)
+{
+  if (deg(poly)<0) return complex<xdouble>(xdouble(0), xdouble(0));
+  complex<xdouble> res(conv<xdouble>(poly[0]), xdouble(0));
+  for (long i=1; i<=deg(poly); i++) {
+    res *= x;
+    res += complex<xdouble>(conv<xdouble>(poly[i]), xdouble(0));
+  }
+  return res;
+}
+complex<xdouble> complexEvalPoly(const DoubleCRT& f,
+                                 const complex<double>& x)
+{
+  ZZX tmp;
+  f.toPoly(tmp);
+  return complexEvalPoly(tmp, x);
+}
+
+// l_2 norm square of canonical embedding
+constexpr double pi = 4 * std::atan(1);
+double embeddingL2NormSquared(const zzX& f, long m)
+{
+  vector<double> acc(m, 0.0);
+  NTL_EXEC_RANGE(m/2, first, last)// no need to compute the second half
+  for (long i=1+first; i<=last; ++i)
+    if (NTL::GCD(i,m)==1) {
+      auto rou = std::polar<double>(1.0, (2*pi*i)/m); // root of unity
+      acc[i] += std::norm(complexEvalPoly(f,rou));
+    }
+  NTL_EXEC_RANGE_END
+  return 2*std::accumulate(acc.begin(), acc.end(), 0.0);
+}
+xdouble embeddingL2NormSquared(const ZZX& f, long m)
+{
+  vector<xdouble> acc(m, xdouble(0.0));
+  NTL_EXEC_RANGE(m/2, first, last)// no need to compute the second half
+  for (long i=1+first; i <=last; ++i) // no need to compute the second half
+    if (NTL::GCD(i,m)==1) {
+      auto rou = std::polar<double>(1.0, (2*pi*i)/m); // root of unity
+      acc[i] += std::norm(complexEvalPoly(f,rou));
+    }
+  NTL_EXEC_RANGE_END
+  return 2*std::accumulate(acc.begin(), acc.end(), xdouble(0.0));
+}
+xdouble embeddingL2NormSquared(const DoubleCRT& f)
+{
+  ZZX tmp;
+  f.toPoly(tmp);
+  return embeddingL2NormSquared(tmp, f.getContext().zMStar.getM());
+}
+
+
+void canonicalEmbedding(std::vector<dcomplex>& v, const zzX& f, long m)
+{
+  cout << "canonicalEmbedding: m="<<m<<", phi(m)="<<phi_N(m)
+       << ", m'th ROU="<<std::polar<double>(1.0, (2*pi)/m)<<endl;
+  v.resize(phi_N(m)/2);
+  long idx = 0;
+  for (long i=1; i <= m/2; ++i) // no need to compute the second half
+    if (NTL::GCD(i,m)==1) {
+      auto rou = std::polar<double>(1.0, (2*pi*i)/m); // root of unity
+      v[idx++] = complexEvalPoly(f,rou);
+    }
+}
+void canonicalEmbedding(std::vector<xcomplex>& v, const NTL::ZZX& f, long m)
+{
+  v.resize(phi_N(m)/2);
+  long idx = 0;
+  for (long i=1; i <= m/2; ++i) // no need to compute the second half
+    if (NTL::GCD(i,m)==1) {
+      auto rou = std::polar<double>(1.0, (2*pi*i)/m); // root of unity
+      v[idx++] = complexEvalPoly(f,rou);
+    }
+}
+void canonicalEmbedding(std::vector<xcomplex>& v, const DoubleCRT& f)
+{
+  ZZX tmp;
+  f.toPoly(tmp);
+  canonicalEmbedding(v, tmp, f.getContext().zMStar.getM());
 }
--- src/norms.h
+++ src/norms.h
@@ -14,11 +14,48 @@
 /**
  * @file norms.h - computing various norms of ring elements
  **/
+#include <complex>
 #include <NTL/ZZX.h>
 #include <NTL/xdouble.h>
 
-NTL::ZZ sumOfCoeffs(const NTL::ZZX& f);  // = f(1)
-NTL::ZZ largestCoeff(const NTL::ZZX& f); // l_infty norm
-NTL::xdouble coeffsL2Norm(const NTL::ZZX& f); // l_2 norm
+long sumOfCoeffs(const zzX& f);         // = f(1)
+NTL::ZZ sumOfCoeffs(const NTL::ZZX& f); // = f(1)
+NTL::ZZ sumOfCoeffs(const DoubleCRT& f); // somewhat lame implementation
 
+//! The L-infinity norm of an element (in coefficient representation)
+long largestCoeff(const zzX& f);         // l_infty norm
+NTL::ZZ largestCoeff(const NTL::ZZX& f); // l_infty norm
+NTL::ZZ largestCoeff(const DoubleCRT& f);
+
+//! The L2-norm of an element (in coefficient representation)
+double coeffsL2NormSquared(const zzX& f); // l2 norm^2
+NTL::xdouble coeffsL2NormSquared(const NTL::ZZX& f); // l2 norm^2
+NTL::xdouble coeffsL2NormSquared(const DoubleCRT& f); // l2 norm^2
+
+inline double coeffsL2Norm(const zzX& f) // l2 norm
+{ return sqrt(coeffsL2NormSquared(f)); }
+inline NTL::xdouble coeffsL2Norm(const NTL::ZZX& f) // l2 norm
+{ return sqrt(coeffsL2NormSquared(f)); }
+inline NTL::xdouble coeffsL2Norm(const DoubleCRT& f) // l2 norm
+{ return sqrt(coeffsL2NormSquared(f)); }
+
+
+//! The L2-norm of the canonical embedding of an element
+double embeddingL2NormSquared(const zzX& f, long m);
+NTL::xdouble embeddingL2NormSquared(const ZZX& f, long m);
+NTL::xdouble embeddingL2NormSquared(const DoubleCRT& f);
+
+typedef complex<double> dcomplex;
+typedef complex<NTL::xdouble> xcomplex;
+
+inline double embeddingL2Norm(const zzX& f, long m)
+{ return sqrt(embeddingL2NormSquared(f,m)); }
+inline NTL::xdouble embeddingL2Norm(const NTL::ZZX& f, long m)
+{ return sqrt(embeddingL2NormSquared(f,m)); }
+inline NTL::xdouble embeddingL2Norm(const DoubleCRT& f)
+{ return sqrt(embeddingL2NormSquared(f)); }
+
+void canonicalEmbedding(std::vector<dcomplex>& v, const zzX& f, long m);
+void canonicalEmbedding(std::vector<xcomplex>& v, const NTL::ZZX& f, long m);
+void canonicalEmbedding(std::vector<xcomplex>& v, const DoubleCRT& f);
 #endif // _NORMS_H_
