64983c82183927798aa09ee49d6a3ae76d93ae54
victorshoup <shoup@cs.nyu.edu>
2018-06-20 16:45:22 -0400

    initial implementation of better digit extraction

--- src/recryption.cpp
+++ src/recryption.cpp
@@ -19,9 +19,13 @@
 #include "intraSlot.h"
 #include "norms.h"
 
+#include "polyEval.h"
+// FIXME: this should move into another module
+// along with extendExtractDigits
+
 long thinRecrypt_initial_level=0;
 
-//#define PRINT_LEVELS
+#define PRINT_LEVELS
 
 
 /************* Some local functions *************/
@@ -825,6 +829,7 @@ void ThinRecryptData::init(const FHEcontext& context, const Vec<long>& mvec_,
 }
 
 
+#if 1
 // Extract digits from thinly packed slots
 void extractDigitsThin(Ctxt& ctxt, long botHigh, long r, long ePrime)
 {
@@ -854,6 +859,10 @@ void extractDigitsThin(Ctxt& ctxt, long botHigh, long r, long ePrime)
     scratch.assign(1, unpacked);
   } else {          // extract digits topHigh...0, store them in scratch
     extractDigits(scratch, unpacked, topHigh+1);
+  }
+
+  for (long i: range(scratch.size())) {
+    CheckCtxt(scratch[i], "**");
   }
 
   // set upacked = -\sum_{j=botHigh}^{topHigh} scratch[j] * p^{j-botHigh}
@@ -892,6 +901,293 @@ void extractDigitsThin(Ctxt& ctxt, long botHigh, long r, long ePrime)
   ctxt = unpacked;
 
 }
+
+#else
+
+// Extract digits from thinly packed slots
+// EXPERIMENTAL VERSION
+
+
+void compute_a_vals(Vec<ZZ>& a, long p, long e)
+// computes a[m] = a(m)/m! for m = p..(e-1)(p-1)+1,
+// as defined by Chen and Han.
+// a.length() is set to (e-1)(p-1)+2
+
+{
+   ZZ p_to_e = power_ZZ(p, e);
+   ZZ p_to_2e = power_ZZ(p, 2*e);
+
+   long len = (e-1)*(p-1)+2;
+
+   ZZ_pPush push(p_to_2e);
+
+   ZZ_pX x_plus_1_to_p = power(ZZ_pX(INIT_MONO, 1) + 1, p);
+   ZZ_pX denom = InvTrunc(x_plus_1_to_p - ZZ_pX(INIT_MONO, p), len);
+   ZZ_pX poly = MulTrunc(x_plus_1_to_p, denom, len);
+   poly *= p;
+
+   a.SetLength(len);
+
+   ZZ m_fac(1);
+   for (long m = 2; m < p; m++) {
+      m_fac = MulMod(m_fac, m, p_to_2e);
+   }
+
+   for (long m = p; m < len; m++) {
+      m_fac = MulMod(m_fac, m, p_to_2e);
+      ZZ c = rep(coeff(poly, m));
+      ZZ d = GCD(m_fac, p_to_2e);
+      if (d == 0 || d > p_to_e || c % d != 0) Error("cannot divide");
+      ZZ m_fac_deflated = (m_fac / d) % p_to_e;
+      ZZ c_deflated = (c / d) % p_to_e;
+      a[m] = MulMod(c_deflated, InvMod(m_fac_deflated, p_to_e), p_to_e);
+   }
+
+}
+
+void compute_magic_poly(ZZX& poly1, long p, long e)
+{
+   FHE_TIMER_START;
+
+   Vec<ZZ> a;
+
+   compute_a_vals(a, p, e);
+
+   ZZ p_to_e = power_ZZ(p, e);
+   long len = (e-1)*(p-1)+2;
+
+   ZZ_pPush push(p_to_e);
+
+   ZZ_pX poly(0);
+   ZZ_pX term(1);
+   ZZ_pX X(INIT_MONO, 1);
+
+   poly = 0;
+   term = 1;
+   
+   for (long m = 0; m < p; m++) {
+      term *= (X-m);
+   }
+
+   for (long m = p; m < len; m++) {
+      poly += term * conv<ZZ_p>(a[m]);
+      term *= (X-m);
+   }
+
+   // replace poly by poly(X+(p-1)/2) for odd p
+   if (p % 2 == 1) {
+      ZZ_pX poly2(0);
+
+      for (long i = deg(poly); i >= 0; i--) 
+         poly2 = poly2*(X+(p-1)/2) + poly[i];
+
+      poly = poly2;
+   }
+
+   poly = X - poly;
+   poly1 = conv<ZZX>(poly);
+}
+
+
+static void buildDigitPolynomial(ZZX& result, long p, long e);
+
+// extendExtractDigits assumes that the slots of *this contains integers mod
+// p^{r+e} i.e., that only the free terms are nonzero. (If that assumptions
+// does not hold then the result will not be a valid ciphertext anymore.)
+// 
+// It returns in the slots of digits[j] the j'th-lowest digits from the
+// integers in the slots of the input. Namely, the i'th slot of digits[j]
+// contains the j'th digit in the p-base expansion of the integer in the i'th
+// slot of the *this.  The plaintext space of digits[j] is mod p^{e+r-j}.
+
+void extendExtractDigits(vector<Ctxt>& digits, const Ctxt& c, long r, long e)
+{
+  const FHEcontext& context = c.getContext();
+
+  long p = context.zMStar.getP();
+  ZZX x2p;
+  if (p>3) { 
+    buildDigitPolynomial(x2p, p, r);
+  }
+
+  // we should pre-compute this table
+  // for i = 0..r-1, entry i is G_{e+r-i} in Chen and Han
+  Vec<ZZX> G;
+  G.SetLength(r);
+  for (long i: range(r)) {
+    compute_magic_poly(G[i], p, e+r-i);
+  }
+
+  vector<Ctxt> digits0;
+
+  Ctxt tmp(c.getPubKey(), c.getPtxtSpace());
+
+  digits.resize(r, tmp);      // allocate space
+  digits0.resize(r, tmp);
+
+  for (long i: range(r)) {
+    tmp = c;
+    for (long j: range(i)) {
+      if (p==2) digits0[j].square();
+      else if (p==3) digits0[j].cube();
+      else polyEval(digits0[j], x2p, digits0[j]); // "in spirit" digits0[j] = digits0[j]^p
+      tmp -= digits0[j];
+      tmp.divideByP();
+    }
+    digits0[i] = tmp; // needed in the next round
+    polyEval(digits[i], G[i], tmp);
+  }
+}
+
+
+// FIXME: this duplicates code
+// Compute a degree-p polynomial poly(x) s.t. for any t<e and integr z of the
+// form z = z0 + p^t*z1 (with 0<=z0<p), we have poly(z) = z0 (mod p^{t+1}).
+//
+// We get poly(x) by interpolating a degree-(p-1) polynomial poly'(x)
+// s.t. poly'(z0)=z0 - z0^p (mod p^e) for all 0<=z0<p, and then setting
+// poly(x) = x^p + poly'(x).
+static void buildDigitPolynomial(ZZX& result, long p, long e)
+{
+  if (p<2 || e<=1) return; // nothing to do
+  FHE_TIMER_START;
+  long p2e = power_long(p,e); // the integer p^e
+
+  // Compute x - x^p (mod p^e), for x=0,1,...,p-1
+  vec_long x(INIT_SIZE, p);
+  vec_long y(INIT_SIZE, p);
+  long bottom = -(p/2);
+  for (long j=0; j<p; j++) {
+    long z = bottom+j;
+    x[j] = z;
+    y[j] = z-PowerMod((z < 0 ? z + p2e : z), p, p2e);  // x - x^p (mod p^e)
+
+    while (y[j] > p2e/2)         y[j] -= p2e;
+    while (y[j] < -(p2e/2))      y[j] += p2e;
+  }
+  interpolateMod(result, x, y, p, e);
+  assert(deg(result)<p); // interpolating p points, should get deg<=p-1
+  SetCoeff(result, p);   // return result = x^p + poly'(x)
+  //  cerr << "# digitExt mod "<<p<<"^"<<e<<"="<<result<<endl;
+  FHE_TIMER_STOP;
+}
+
+
+
+
+void extractDigitsThin(Ctxt& ctxt, long botHigh, long r, long ePrime)
+{
+  FHE_TIMER_START;
+
+  Ctxt unpacked(ctxt);
+  unpacked.cleanUp();
+
+  vector<Ctxt> scratch;
+
+
+  // Step 2: extract the digits top-1,...,0 from the slots of unpacked[i]
+  long p = ctxt.getContext().zMStar.getP();
+  long p2r = power_long(p,r);
+  long topHigh = botHigh + r-1;
+
+  if (r > 1) {
+
+#ifdef DEBUG_PRINTOUT
+    cerr << "+ After unpack ";
+    decryptAndPrint(cerr, unpacked, *dbgKey, *dbgEa, printFlag);
+    cerr << "    extracting "<<(topHigh+1)<<" digits\n";
+#endif
+
+    extendExtractDigits(scratch, unpacked, botHigh, r);
+
+    for (long i: range(scratch.size())) {
+      CheckCtxt(scratch[i], "**");
+    }
+
+    for (long j = 0; j < botHigh; j++) {
+      unpacked -= scratch[j];
+      unpacked.divideByP();
+    }
+
+    if (p==2 && botHigh>0)   // For p==2, subtract also the previous bit
+      unpacked += scratch[botHigh-1];
+    unpacked.negate();
+
+    if (r>ePrime) {          // Add in digits from the bottom part, if any
+      long topLow = r-1 - ePrime;
+      Ctxt tmp = scratch[topLow];
+      for (long j=topLow-1; j>=0; --j) {
+	tmp.multByP();
+	tmp += scratch[j];
+      }
+      if (ePrime>0)
+	tmp.multByP(ePrime); // multiply by p^e'
+      unpacked += tmp;
+    }
+    unpacked.reducePtxtSpace(p2r); // Our plaintext space is now mod p^r
+
+#ifdef DEBUG_PRINTOUT
+    cerr << "+ Before repack ";
+    decryptAndPrint(cerr, unpacked[0], *dbgKey, *dbgEa, printFlag);
+#endif
+
+    ctxt = unpacked;
+
+  }
+  else {
+#ifdef DEBUG_PRINTOUT
+    cerr << "+ After unpack ";
+    decryptAndPrint(cerr, unpacked, *dbgKey, *dbgEa, printFlag);
+    cerr << "    extracting "<<(topHigh+1)<<" digits\n";
+#endif
+
+    if (p==2 && r>2)
+      topHigh--; // For p==2 we sometime get a bit for free
+
+    if (topHigh<=0) { // extracting LSB = no-op
+      scratch.assign(1, unpacked);
+    } else {          // extract digits topHigh...0, store them in scratch
+      extractDigits(scratch, unpacked, topHigh+1);
+    }
+
+    // set upacked = -\sum_{j=botHigh}^{topHigh} scratch[j] * p^{j-botHigh}
+    if (topHigh >= LONG(scratch.size())) {
+      topHigh = scratch.size() -1;
+      cerr << " @ suspect: not enough digits in extractDigitsPacked\n";
+    }
+
+    unpacked = scratch[topHigh];
+    for (long j=topHigh-1; j>=botHigh; --j) {
+      unpacked.multByP();
+      unpacked += scratch[j];
+    }
+    if (p==2 && botHigh>0)   // For p==2, subtract also the previous bit
+      unpacked += scratch[botHigh-1];
+    unpacked.negate();
+
+    if (r>ePrime) {          // Add in digits from the bottom part, if any
+      long topLow = r-1 - ePrime;
+      Ctxt tmp = scratch[topLow];
+      for (long j=topLow-1; j>=0; --j) {
+	tmp.multByP();
+	tmp += scratch[j];
+      }
+      if (ePrime>0)
+	tmp.multByP(ePrime); // multiply by p^e'
+      unpacked += tmp;
+    }
+    unpacked.reducePtxtSpace(p2r); // Our plaintext space is now mod p^r
+
+#ifdef DEBUG_PRINTOUT
+    cerr << "+ Before repack ";
+    decryptAndPrint(cerr, unpacked[0], *dbgKey, *dbgEa, printFlag);
+#endif
+
+    ctxt = unpacked;
+  }
+
+}
+#endif
 
 
 // Hack to get at private fields of public key
