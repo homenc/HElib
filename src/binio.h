f905e95151985b64fe80ac6bc9666b8432f01617
Shai Halevi <shaih@us.ibm.com>
2018-07-06 06:45:02 -0400

    fixed dependencies

--- src/binio.h
+++ src/binio.h
@@ -9,12 +9,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. See accompanying LICENSE file.
  */
-
 #ifndef  _BINIO_H_
 #define  _BINIO_H_
-
+#include <iostream>
+#include <vector>
 #include <type_traits>
-#include "FHE.h"
+#include <NTL/xdouble.h>
+#include <NTL/vec_long.h>
 
 #define BINIO_32BIT 4
 #define BINIO_48BIT 6
@@ -45,22 +46,22 @@
 
 /* Some utility functions for binary IO */
 
-int readEyeCatcher(istream& str, const char * expect);
-void writeEyeCatcher(ostream& str, const char* eye);
+int readEyeCatcher(std::istream& str, const char * expect);
+void writeEyeCatcher(std::ostream& str, const char* eye);
 
-void write_ntl_vec_long(ostream& str, const vec_long& vl, long intSize=BINIO_64BIT);
-void read_ntl_vec_long(istream& str, vec_long& vl);
+void write_ntl_vec_long(std::ostream& str, const NTL::vec_long& vl, long intSize=BINIO_64BIT);
+void read_ntl_vec_long(std::istream& str, NTL::vec_long& vl);
 
-long read_raw_int(istream& str, long intSize=BINIO_64BIT);
-void write_raw_int(ostream& str, long num, long intSize=BINIO_64BIT);
+long read_raw_int(std::istream& str, long intSize=BINIO_64BIT);
+void write_raw_int(std::ostream& str, long num, long intSize=BINIO_64BIT);
 
-void write_raw_xdouble(ostream& str, const xdouble xd);
-xdouble read_raw_xdouble(istream& str);
+void write_raw_xdouble(std::ostream& str, const NTL::xdouble xd);
+NTL::xdouble read_raw_xdouble(std::istream& str);
 
-void write_raw_ZZ(ostream& str, const ZZ& zz);
-void read_raw_ZZ(istream& str, ZZ& zz);
+void write_raw_ZZ(std::ostream& str, const NTL::ZZ& zz);
+void read_raw_ZZ(std::istream& str, NTL::ZZ& zz);
 
-template<typename T> void write_raw_vector(ostream& str, const vector<T>& v)
+template<typename T> void write_raw_vector(std::ostream& str, const std::vector<T>& v)
 {
   long sz = v.size(); 
   write_raw_int(str, v.size()); 
@@ -70,9 +71,9 @@ template<typename T> void write_raw_vector(ostream& str, const vector<T>& v)
   }
 };
 // vector<long> has adifferent implementation, since long.write does not work
-template<> void write_raw_vector<long>(ostream& str, const vector<long>& v);
+template<> void write_raw_vector<long>(std::ostream& str, const std::vector<long>& v);
 
-template<typename T> void read_raw_vector(istream& str, vector<T>& v, T& init)
+template<typename T> void read_raw_vector(std::istream& str, std::vector<T>& v, T& init)
 {
   long sz = read_raw_int(str);
   v.resize(sz, init); // Make space in vector
@@ -82,15 +83,16 @@ template<typename T> void read_raw_vector(istream& str, vector<T>& v, T& init)
   }
 };
 
-template<typename T> void read_raw_vector(istream& str, vector<T>& v)
+template<typename T> void read_raw_vector(std::istream& str, std::vector<T>& v)
 {
   read_raw_vector<T>(str, v, T());
 }
 // vector<long> has adifferent implementation, since long.read does not work
-template<> void read_raw_vector<long>(istream& str, vector<long>& v);
+template<> void read_raw_vector<long>(std::istream& str, std::vector<long>& v);
 
 // KeySwitch::read(...) (in FHE.cpp) requires the context.
-template<typename T> void read_raw_vector(istream& str, vector<T>& v, const FHEcontext& context)
+class FHEcontext;
+template<typename T> void read_raw_vector(std::istream& str, std::vector<T>& v, const FHEcontext& context)
 { 
   long sz = read_raw_int(str);
   v.resize(sz); // Make space in vector
