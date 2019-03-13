801a43f791cb4ae72ce4ca99bba8c712eac442d7
Oliver Masters <oliver.masters@ibm.com>
2019-03-13 11:34:45 +0000

    Improve version checking for GMP and Armadillo

--- cmake/get_gmp_version.c
+++ cmake/get_gmp_version.c
@@ -6,6 +6,6 @@ int main(void)
   const unsigned gmp_version = __GNU_MP_VERSION;
   const unsigned gmp_version_minor = __GNU_MP_VERSION_MINOR;
   const unsigned gmp_version_patchlevel = __GNU_MP_VERSION_PATCHLEVEL;
-  printf("%d.%d.%d\n", gmp_version, gmp_version_minor, gmp_version_patchlevel);
+  printf("%d.%d.%d", gmp_version, gmp_version_minor, gmp_version_patchlevel);
   return 0;
 }
