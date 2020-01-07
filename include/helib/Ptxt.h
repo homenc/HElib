8b2b8e8dccb2ce60a4632d9e336541c3e0beee5b
Jack L Crawford <Jack.Crawford@ibm.com>
2020-01-07 17:29:59 +0000

    *= operator for Ctxt for constant multiplication (#259)

--- include/helib/Ptxt.h
+++ include/helib/Ptxt.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019 IBM Corp.
+/* Copyright (C) 2019-2020 IBM Corp.
  * This program is Licensed under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
--- src/Ctxt.cpp
+++ src/Ctxt.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012-2019 IBM Corp.
+/* Copyright (C) 2012-2020 IBM Corp.
  * This program is Licensed under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
@@ -861,6 +861,20 @@ Ctxt& Ctxt::operator*=(const helib::Ptxt<helib::CKKS>& other)
 {
   multByConstantCKKS(other);
   return *this;
+}
+
+Ctxt& Ctxt::operator*=(const NTL::ZZX& poly)
+{
+  if(isCKKS())
+    multByConstantCKKS(poly);
+  else
+    multByConstant(poly);
+  return *this;
+}
+
+Ctxt& Ctxt::operator*=(const long scalar)
+{
+  return *this *= NTL::ZZX(scalar);
 }
 
 void Ctxt::addConstantCKKS(const std::vector<std::complex<double>>& other)
