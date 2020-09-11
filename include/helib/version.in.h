bd1a80ea4947bc2290efb06e53eb8dcc84917e57
hamishun <60481960+hamishun@users.noreply.github.com>
2020-09-12 00:52:25 +0100

    Adding a version.{h,cpp}
    - Simple compile time version to headers.
    - Generated by cmake - so one less place to update.
    - Also embedded version in the compiled library in case one needs sanity check.
    
    Co-authored-by: Enrico Steffinlongo <enrylongo@gmail.com>

---
+++ include/helib/version.in.h
@@ -0,0 +1,78 @@
+/* Copyright (C) 2020 IBM Corp.
+ * This program is Licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+
+#ifndef HELIB_VERSION_H
+#define HELIB_VERSION_H
+
+namespace helib {
+
+/**
+ * @class version
+ * @brief The class acts as a namespace with all members static.
+ * Holds the version number for this code of HElib.
+ **/
+struct version
+{
+
+  // clang-format off
+  /**
+   * @brief The major number of this version of HElib.
+   **/
+  static constexpr long major = @PROJECT_VERSION_MAJOR@;
+  /**
+   * @brief The minor number of this version of HElib.
+   **/
+  static constexpr long minor = @PROJECT_VERSION_MINOR@;
+  /**
+   * @brief The patch number of this version of HElib.
+   **/
+  static constexpr long patch = @PROJECT_VERSION_PATCH@;
+  // clang-format on
+
+  /**
+   * @brief The string representation of this version of HElib.
+   **/
+  static constexpr auto asString = "v@PROJECT_VERSION@";
+
+  /**
+   * @brief Function that returns whether this version of HElib is equal to or
+   * higher than a specified version.
+   * @param major The major version number.
+   * @param minor The minor version number.
+   * @param patch The patch version number.
+   * @return `true` if current HElib version is greater than or equal to the
+   * specified version version.
+   **/
+  static inline constexpr bool greaterEquals(long major_,
+                                             long minor_ = 0,
+                                             long patch_ = 0)
+  {
+    if (major_ < 0 || minor_ < 0 || patch_ < 0)
+      return false;
+    long min_version = (((major_ << 8) ^ minor_) << 8) ^ patch_;
+    long our_version = (((major << 8) ^ minor) << 8) ^ patch;
+    return our_version >= min_version;
+  }
+
+  /**
+   * @brief Get the string version from the HElib compiled library instead of
+   * the one defined in the header.
+   * @return A string representing the version of HElib stored in the compiled
+   * library instead of from the one defined in the header.
+   **/
+  static const char* libString();
+
+}; // struct version
+
+} // namespace helib
+
+#endif // HELIB_VERSION_H
---
+++ tests/TestVersion.in.cpp
@@ -0,0 +1,107 @@
+/* Copyright (C) 2020 IBM Corp.
+ * This program is Licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+
+#include <helib/version.h>
+#include <tuple>
+#include <string>
+#include <regex>
+#include <sstream>
+#include <fstream>
+#include "test_common.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+// Util function to grab version from cmake file.
+std::tuple<int, int, int, std::string> readVersionFromCmakeFile()
+{
+  // File will be in src/, we are in tests/
+  const std::string cmakeFilePath = "@CMAKE_SOURCE_DIR@/src/CMakeLists.txt";
+
+  // Slurp the file. It isn't a large file.
+  std::string fileStr;
+
+  {
+    std::ifstream cmakeFile(cmakeFilePath);
+    if (!cmakeFile.is_open())
+      throw std::runtime_error("Could not find '" + cmakeFilePath + "'.");
+
+    std::ostringstream oss;
+    oss << cmakeFile.rdbuf();
+    fileStr = oss.str();
+  }
+
+  // Find the version.
+  // e.g.
+  // project(helib
+  //         VERSION x.y.z
+  //         LANGUAGES CXX)
+
+  std::regex re_version(
+      R"(project[\s\S]*?helib[\s\S]*?VERSION[\s\S]*?((\d+)\.(\d+)\.(\d)))");
+  std::smatch match;
+  std::regex_search(fileStr, match, re_version);
+  if (match.size() < 5) {
+    std::ostringstream oss;
+    oss << "Expected 5 matches, got " << match.size() << ".";
+    throw std::runtime_error(oss.str());
+  }
+
+  return std::tuple<int, int, int, std::string>(std::stoi(match[2]),
+                                                std::stoi(match[3]),
+                                                std::stoi(match[4]),
+                                                "v" + match[1].str());
+}
+
+TEST(TestVersion, versionMatchesThatFoundInCMakelists)
+{
+  int major;
+  int minor;
+  int patch;
+  std::string verStr;
+
+  std::tie(major, minor, patch, verStr) = readVersionFromCmakeFile();
+
+  // EXPECT_EQ does not like these class static members.
+  int cur_maj = helib::version::major;
+  int cur_min = helib::version::minor;
+  int cur_pat = helib::version::patch;
+
+  EXPECT_EQ(cur_maj, major);
+  EXPECT_EQ(cur_min, minor);
+  EXPECT_EQ(cur_pat, patch);
+  EXPECT_STREQ(helib::version::asString, verStr.c_str());
+}
+
+TEST(TestVersion, versionGreaterThanOrEqualTo)
+{
+  EXPECT_FALSE(helib::version::greaterEquals(3));
+  EXPECT_FALSE(helib::version::greaterEquals(3, 1));
+  EXPECT_FALSE(helib::version::greaterEquals(3, 1, 4));
+  // Shouldn't work for negative numbers
+  EXPECT_FALSE(helib::version::greaterEquals(1, -1, 0));
+  EXPECT_FALSE(helib::version::greaterEquals(0, -1, 0));
+  // Older version
+  EXPECT_TRUE(helib::version::greaterEquals(1, 0, 0));
+  // Current version
+  // clang-format off
+  EXPECT_TRUE(helib::version::greaterEquals(@PROJECT_VERSION_MAJOR@, @PROJECT_VERSION_MINOR@, @PROJECT_VERSION_PATCH@));
+  // clang-format on
+}
+
+TEST(TestVersion, versionLibString)
+{
+  const char* libString = helib::version::libString();
+  EXPECT_STREQ(libString, "v@PROJECT_VERSION@");
+}
+
+} // namespace
