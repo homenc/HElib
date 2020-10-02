9818ea4d6dd623fb60c3c8ec2b8ce0ea04ba29ba
Flavio Bergamaschi <anadyomeni@gmx.com>
2020-10-02 17:59:48 -0400

    HElib v1.1.0 Documentation update

---
+++ search/classes_15.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['zz_5fpxmodulus1_1445',['ZZ_pXModulus1',['../classhelib_1_1_z_z__p_x_modulus1.html',1,'helib::ZZ_pXModulus1'],['../classhelib_1_1zz__p_x_modulus1.html',1,'helib::zz_pXModulus1']]]
+];
