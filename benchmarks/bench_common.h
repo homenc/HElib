613fa0ce2df1dcf8695a39d994d7540c61b449d5
Hamish Hunt <hamishun@gmail.com>
2021-08-06 08:30:06 -0700

    final move to 1.2.0. Changed to ns time for bench

--- benchmarks/bench_common.h
+++ benchmarks/bench_common.h
@@ -2,7 +2,7 @@
 #define HELIB_BENCH_COMMON_H
 
 #define HE_BENCH_CAPTURE(adding_two_ciphertexts, tiny_params, fn)              \
-  BENCHMARK_CAPTURE(adding_two_ciphertexts, tiny_params, fn(tiny_params))      \
-      ->Unit(benchmark::kMillisecond)
+  BENCHMARK_CAPTURE(adding_two_ciphertexts, tiny_params, fn(tiny_params))      
+
 
 #endif // HELIB_BENCH_COMMON_H
