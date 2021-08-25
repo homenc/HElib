53e2846bb1de2f589a6085a15e39d47f46062d51
Moritz Eck <moritz.eck@gmail.com>
2021-08-25 15:48:58 +0200

    refactor + add func length_encrypted_db_key

--- examples/BGV_country_db_lookup/BGV_string_int_db_lookup.cpp
+++ examples/BGV_country_db_lookup/BGV_string_int_db_lookup.cpp
@@ -5,18 +5,6 @@
 #include <helib/ArgMap.h>
 #include <NTL/BasicThreadPool.h>
 
-// Utility function to print polynomials
-void printPoly(NTL::ZZX& poly)
-{
-  for (int i = NTL::deg(poly); i >= 0; i--) {
-    std::cout << poly[i] << "x^" << i;
-    if (i > 0)
-      std::cout << " + ";
-    else
-      std::cout << "\n";
-  }
-}
-
 // Utility function to read <K,V> CSV data from file
 std::vector<std::pair<std::string, int>> read_csv(std::string filename)
 {
@@ -47,6 +35,70 @@ std::vector<std::pair<std::string, int>> read_csv(std::string filename)
 
   data_file.close();
   return dataset;
+}
+
+std::vector<std::pair<helib::Ctxt, helib::Ctxt>> setup_database(
+    std::vector<std::pair<std::string, int>> db,
+    const helib::PubKey& public_key,
+    helib::Context& context,
+    bool debug = true)
+{
+  // Convert strings into numerical vectors
+  std::cout << "\n---Initializing the encrypted key,value pair database ("
+            << db.size() << " entries)...";
+  std::cout
+      << "\nConverting strings to numeric representation into Ptxt objects ..."
+      << std::endl;
+
+  // Generating the Plain text representation of Country DB
+  HELIB_NTIMER_START(timer_PtxtCountryDB);
+  std::vector<std::pair<helib::Ptxt<helib::BGV>, helib::Ptxt<helib::BGV>>>
+      db_ptxt;
+
+  for (const auto& key_value_pair : db) {
+    if (debug) {
+      std::cout << "\t\tname_addr_pair.first size = "
+                << key_value_pair.first.size() << " (" << key_value_pair.first
+                << ")"
+                << "\tname_addr_pair.second"
+                << "(" << key_value_pair.second << ")" << std::endl;
+    }
+
+    helib::Ptxt<helib::BGV> country(context);
+    std::cout << "\tname size = " << country.size() << std::endl;
+    for (long i = 0; i < key_value_pair.first.size(); ++i) {
+      country.at(i) = key_value_pair.first[i];
+    }
+
+    helib::Ptxt<helib::BGV> capital(context);
+    capital.addConstant(key_value_pair.second);
+
+    db_ptxt.emplace_back(std::move(country), std::move(capital));
+  }
+  HELIB_NTIMER_STOP(timer_PtxtCountryDB);
+
+  // Encrypt the Country DB
+  std::cout << "Encrypting the database..." << std::endl;
+  HELIB_NTIMER_START(timer_CtxtCountryDB);
+  std::vector<std::pair<helib::Ctxt, helib::Ctxt>> encrypted_db;
+  for (const auto& key_value_pair : db_ptxt) {
+    helib::Ctxt encrypted_key(public_key);
+    helib::Ctxt encrypted_value(public_key);
+    public_key.Encrypt(encrypted_key, key_value_pair.first);
+    public_key.Encrypt(encrypted_value, key_value_pair.second);
+    encrypted_db.emplace_back(std::move(encrypted_key),
+                              std::move(encrypted_value));
+  }
+  HELIB_NTIMER_STOP(timer_CtxtCountryDB);
+
+  return encrypted_db;
+}
+
+helib::Ctxt length_encrypted_db_key(helib::Ctxt encrypted_key, unsigned long p)
+{
+  encrypted_key.power(p - 1);
+  totalSums(encrypted_key);
+  return encrypted_key;
 }
 
 int main(int argc, char* argv[])
@@ -142,8 +194,9 @@ int main(int argc, char* argv[])
 
   // Print the context
   std::cout << std::endl;
-  if (debug)
+  if (debug) {
     context.printout();
+  }
 
   // Print the security level
   // Note: This will be negligible to improve performance time.
@@ -163,54 +216,8 @@ int main(int argc, char* argv[])
     exit(1);
   }
 
-  // Convert strings into numerical vectors
-  std::cout << "\n---Initializing the encrypted key,value pair database ("
-            << country_db.size() << " entries)...";
-  std::cout
-      << "\nConverting strings to numeric representation into Ptxt objects ..."
-      << std::endl;
-
-  // Generating the Plain text representation of Country DB
-  HELIB_NTIMER_START(timer_PtxtCountryDB);
-  std::vector<std::pair<helib::Ptxt<helib::BGV>, helib::Ptxt<helib::BGV>>>
-      country_db_ptxt;
-  for (const auto& country_capital_pair : country_db) {
-    if (debug) {
-      std::cout << "\t\tname_addr_pair.first size = "
-                << country_capital_pair.first.size() << " ("
-                << country_capital_pair.first << ")"
-                << "\tname_addr_pair.second"
-                << "(" << country_capital_pair.second << ")" << std::endl;
-    }
-
-    helib::Ptxt<helib::BGV> country(context);
-    std::cout << "\tname size = " << country.size() << std::endl;
-    for (long i = 0; i < country_capital_pair.first.size(); ++i) {
-      country.at(i) = country_capital_pair.first[i];
-    }
-
-    // TODO: figure out how to encode int into Ptxt
-    helib::Ptxt<helib::BGV> capital(context);
-    capital.addConstant(country_capital_pair.second);
-
-    country_db_ptxt.emplace_back(std::move(country), std::move(capital));
-  }
-  HELIB_NTIMER_STOP(timer_PtxtCountryDB);
-
-  // Encrypt the Country DB
-  std::cout << "Encrypting the database..." << std::endl;
-  HELIB_NTIMER_START(timer_CtxtCountryDB);
-  std::vector<std::pair<helib::Ctxt, helib::Ctxt>> encrypted_country_db;
-  for (const auto& country_capital_pair : country_db_ptxt) {
-    helib::Ctxt encrypted_country(public_key);
-    helib::Ctxt encrypted_capital(public_key);
-    public_key.Encrypt(encrypted_country, country_capital_pair.first);
-    public_key.Encrypt(encrypted_capital, country_capital_pair.second);
-    encrypted_country_db.emplace_back(std::move(encrypted_country),
-                                      std::move(encrypted_capital));
-  }
-
-  HELIB_NTIMER_STOP(timer_CtxtCountryDB);
+  std::vector<std::pair<helib::Ctxt, helib::Ctxt>> encrypted_country_db =
+      setup_database(country_db, public_key, context);
 
   // Print DB Creation Timers
   if (debug) {
@@ -230,10 +237,10 @@ int main(int argc, char* argv[])
 
   // Read in query from the command line
   std::string query_string;
-  std::cout << "\nPlease enter the name of an European Country: ";
+  std::cout << "\nPlease enter a valid name: ";
   // std::cin >> query_string;
   std::getline(std::cin, query_string);
-  std::cout << "Looking for the Capital of " << query_string << std::endl;
+  std::cout << "Looking for the value of name: " << query_string << std::endl;
   std::cout << "This may take few minutes ... " << std::endl;
 
   HELIB_NTIMER_START(timer_TotalQuery);
@@ -258,14 +265,20 @@ int main(int argc, char* argv[])
   mask.reserve(country_db.size());
   for (const auto& encrypted_pair : encrypted_country_db) {
     helib::Ctxt mask_entry = encrypted_pair.first; // Copy of database key
-    mask_entry -= query;                           // Calculate the difference
-    mask_entry.power(p - 1);                       // Fermat's little theorem
-    mask_entry.negate();                           // Negate the ciphertext
-    mask_entry.addConstant(NTL::ZZX(1));           // 1 - mask = 0 or 1
+
+    helib::Ctxt mask_entry_length = length_encrypted_db_key(mask_entry, p);
+
+    mask_entry -= query;                 // Calculate the difference
+    mask_entry.power(p - 1);             // Fermat's little theorem
+    mask_entry.negate();                 // Negate the ciphertext
+    mask_entry.addConstant(NTL::ZZX(1)); // 1 - mask = 0 or 1
+
     // Create a vector of copies of the mask
     std::vector<helib::Ctxt> rotated_masks(ea.size(), mask_entry);
-    for (int i = 1; i < rotated_masks.size(); i++)
-      ea.rotate(rotated_masks[i], i);             // Rotate each of the masks
+    for (int i = 1; i < rotated_masks.size(); i++) {
+      ea.rotate(rotated_masks[i], i); // Rotate each of the masks
+    }
+
     totalProduct(mask_entry, rotated_masks);      // Multiply each of the masks
     mask_entry.multiplyBy(encrypted_pair.second); // multiply mask with values
     mask.push_back(mask_entry);
@@ -289,11 +302,16 @@ int main(int argc, char* argv[])
   HELIB_NTIMER_STOP(timer_DecryptQueryResult);
 
   // extract first element as the result
-  int result;
-  result = static_cast<long>(plaintext_result[0]);
-  std::cout << "query result: " << result << std::endl;
-
+  std::string query_result =
+      std::to_string(static_cast<long>(plaintext_result[0]));
   HELIB_NTIMER_STOP(timer_TotalQuery);
+
+  if (query_result == "0") {
+    query_result = "Name not in the database."
+                   "\n*** Please make sure to enter a valid name"
+                   "\n*** with the first letter in upper case.";
+  }
+  std::cout << "\nQuery result: " << query_result << std::endl;
 
   // Print DB Query Timers
   if (debug) {
@@ -303,13 +321,6 @@ int main(int argc, char* argv[])
     std::cout << std::endl;
   }
 
-  // if (string_result.at(0) == 0x00) {
-  //   string_result =
-  //       "Country name not in the database."
-  //       "\n*** Please make sure to enter the name of a European Country"
-  //       "\n*** with the first letter in upper case.";
-  // }
-  // std::cout << "\nQuery result: " << string_result << std::endl;
   helib::printNamedTimer(std::cout, "timer_TotalQuery");
 
   return 0;
