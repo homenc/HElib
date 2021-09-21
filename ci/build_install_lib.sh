fc1033321cb762a84f52a077a4696802a37fad01
Fabian Boemer <fabian.boemer@intel.com>
2021-09-21 07:07:52 -0700

    GitHub actions CI

--- ci/build_install_lib.sh
+++ ci/build_install_lib.sh
@@ -11,21 +11,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License. See accompanying LICENSE file.
 
+# Copyright (C) 2021 Intel Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -xe
 
-if [ "$#" -ne 2 ]; then
-  echo "Wrong parameter number. Usage ./${0} <PACKAGE_BUILD> <CMAKE_INSTALL_PREFIX>"
+if [ "$#" -ne 6 ]; then
+  echo "Wrong parameter number. Usage ./${0} <PACKAGE_BUILD> <CMAKE_INSTALL_PREFIX> <C_COMPILER> <CXX_COMPILER> <USE_INTEL_HEXL> <INTEL_HEXL_DIR>"
   exit 1
 fi
 
 PACKAGE_BUILD="${1}"
 CMAKE_INSTALL_PREFIX="${2}"
+C_COMPILER="${3}"
+CXX_COMPILER="${4}"
+USE_INTEL_HEXL="${5}"
+INTEL_HEXL_DIR="${6}"
 ROOT_DIR="$(pwd)"
 
 mkdir build
 cd build
 # We assume PACKAGE_BUILD argument to be a valid cmake option
-cmake -DPACKAGE_BUILD="${PACKAGE_BUILD}" -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" -DBUILD_SHARED=ON -DENABLE_TEST=ON -DTARGET_ARCHITECTURE=x86-64 ..
+cmake -DPACKAGE_BUILD="${PACKAGE_BUILD}" \
+      -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" \
+      -DUSE_INTEL_HEXL="${USE_INTEL_HEXL}" \
+      -DCMAKE_C_COMPILER="${C_COMPILER}" \
+      -DCMAKE_CXX_COMPILER="${CXX_COMPILER}" \
+      -DHEXL_DIR="${INTEL_HEXL_DIR}" \
+      -DBUILD_SHARED=OFF \
+      -DENABLE_TEST=ON \
+      -DTARGET_ARCHITECTURE=x86-64 \
+      ..
 make -j4 VERBOSE=1
 make install
 cd "${ROOT_DIR}"
--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -11,33 +11,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License. See accompanying LICENSE file.
 
+# Copyright (C) 2021 Intel Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -xe
+
+if [ "$#" -ne 5 ]; then
+  echo "Wrong parameter number. Usage ./${0} <PACKAGE_BUILD> <RUNNER_OS> <C_COMPILER> <CXX_COMPILER> <USE_INTEL_HEXL>"
+  exit 1
+fi
+
+PACKAGE_BUILD="${1}"
+RUNNER_OS="${2}"
+C_COMPILER="${3}"
+CXX_COMPILER="${4}"
+USE_INTEL_HEXL="${5}"
+
+if [ "${USE_INTEL_HEXL}" == "ON" ]; then
+  git clone https://github.com/intel/hexl.git -b v1.2.1
+  cd hexl
+  cmake -B build \
+    -DCMAKE_INSTALL_PREFIX=./ \
+    -DHEXL_SHARED_LIB=OFF \
+    -DCMAKE_C_COMPILER="${C_COMPILER}" \
+    -DCMAKE_CXX_COMPILER="${CXX_COMPILER}"
+  cmake --build build -j4 --target install
+  cd ../
+fi
 
 cd $HOME
 
-if [ "${PACKAGE_BUILD}" == "OFF" ]; then
-  if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-    if [ "${TRAVIS_DIST}" == "bionic" ]; then
-      sudo apt-get -yq --no-install-suggests --no-install-recommends $(travis_apt_get_options) install m4 libgmp-dev
-      curl -O "https://libntl.org/ntl-11.4.3.tar.gz"
-      tar --no-same-owner -xf ntl-11.4.3.tar.gz
-      cd "$HOME/ntl-11.4.3/src"
-      ./configure SHARED=on NTL_GMP_LIP=on NTL_THREADS=on NTL_THREAD_BOOST=on
-      make -j4
-      sudo make install
-    else
-      sudo apt-get -yq --no-install-suggests --no-install-recommends $(travis_apt_get_options) install libgmp-dev libntl-dev
-    fi
-  elif [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-    # GMP will be installed as a dependency to NTL (if it is not already present)
-    brew install ntl
-  fi
-else
-  if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-    sudo apt-get -yq --no-install-suggests --no-install-recommends $(travis_apt_get_options) install patchelf m4
-  elif [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-    brew install m4
-  fi
+if [ "${RUNNER_OS}" == "ubuntu-20.04" ]; then
+    sudo apt-get -yq --no-install-suggests --no-install-recommends install libgmp-dev libntl-dev bats
+fi
+
+if [ "${RUNNER_OS}" == "macos-latest" ]; then
+  brew install ntl
 fi
 
 cd "$HOME"
